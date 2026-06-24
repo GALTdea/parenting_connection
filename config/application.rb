350ee1fdef91518bbee5811896581c7646a3b6ee
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2026-06-24 11:59:14 -0700

    Fix module name casing in application.rb to follow CamelCase convention

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module parenting_connection_app
+module ParentingConnectionApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
